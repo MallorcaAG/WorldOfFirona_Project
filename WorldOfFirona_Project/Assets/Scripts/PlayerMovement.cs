033e3343624afe4fa09e41d5d879a0b00ca0b074
PhoenixHotware <firebirbgamer@gmail.com>
2024-02-08 10:08:53 +0800

    Fixed stamina script

--- WorldOfFirona_Project/Assets/Scripts/PlayerMovement.cs
+++ WorldOfFirona_Project/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-
     [Header("Movement")]
     private float movementSpeed;
     [SerializeField] private float walkSpeed;
@@ -35,6 +34,15 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDir;
 
     Rigidbody rb;
+
+    public Image StaminaBar;
+
+    public float Stamina, MaxStamina;
+
+    public float RunCost;
+    public float ChargeRate;
+
+    private Coroutine recharge;
 
     [SerializeField] private MovementState state;
     public enum MovementState
@@ -99,6 +107,13 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.sprinting;
             movementSpeed = runSpeed;
+
+            Stamina -= RunCost * Time.deltaTime;
+            if (Stamina < 0) Stamina = 0;
+            StaminaBar.fillAmount = Stamina / MaxStamina;
+
+            if (recharge != null) StopCoroutine(recharge);
+            recharge = StartCoroutine(RechargeStamina());
         }
         else if(grounded)
         {
@@ -150,5 +165,18 @@ public class PlayerMovement : MonoBehaviour
     {
         
         canJump = true;
+    }
+
+    private IEnumerator RechargeStamina()
+    {
+        yield return new WaitForSeconds(1f);
+
+        while(Stamina < MaxStamina)
+        {
+            Stamina += ChargeRate / 10f;
+            if (Stamina > MaxStamina) Stamina = MaxStamina;
+            StaminaBar.fillAmount = Stamina / MaxStamina;
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
